34faaf7d12681c17e92179521673b467a1ca85d0
aliyar <aliyrabdrasitov112@gmail.com>
2023-01-31 08:46:29 +0600

    fix bu

--- Spotify_hakaton/Spotify_hakaton/celery.py
+++ Spotify_hakaton/Spotify_hakaton/celery.py
@@ -1,21 +1,21 @@
-from __future__ import absolute_import
 import os
-from celery import Celery
 
-# этот код скопирован с manage.py
-# он установит модуль настроек по умолчанию Django для приложения 'celery'.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_django.settings')
+from celery import  Celery
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spotify_hakaton.settings')
 
-# здесь вы меняете имя
-app = Celery("celery_django")
+app = Celery('Spotify')
 
-# Для получения настроек Django, связываем префикс "CELERY" с настройкой celery
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# загрузка tasks.py в приложение django
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 
-@app.task
-def add(x, y):
-    return x / y
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
